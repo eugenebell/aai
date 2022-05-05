552da80b745178cac8c0b686692ef5b1f4ee01ee
Eugene Bell2 <ebell@ebell-a01.vmware.com>
2022-05-05 17:26:11 +0100

    db

--- src/main/java/ageaction/DatabaseConfig.java
+++ src/main/java/ageaction/DatabaseConfig.java
@@ -4,6 +4,7 @@ import com.zaxxer.hikari.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
+import org.springframework.orm.hibernate5.*;
 
 @Configuration
 public class DatabaseConfig {
