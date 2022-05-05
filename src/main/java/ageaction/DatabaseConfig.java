f9f70f1644b7a2d9473221b44fc39010a771341c
Eugene Bell2 <ebell@ebell-a01.vmware.com>
2022-05-05 17:36:09 +0100

    db

--- src/main/java/ageaction/DatabaseConfig.java
+++ src/main/java/ageaction/DatabaseConfig.java
@@ -6,23 +6,8 @@ import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 import org.springframework.orm.hibernate5.*;
 
-@Configuration
+//@Configuration
 public class DatabaseConfig {
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
-
-  @Bean
-  public DataSource dataSource() {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-  }
-
-  @Bean(name="entityManagerFactory")
-public LocalSessionFactoryBean sessionFactory() {
-    LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-
-    return sessionFactory;
-} 
+//    
 }
